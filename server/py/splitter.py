389898fdf39636e7b4ba6747f499c0d94e9cb9f1
Bruno de Marco <bruno.appolonio@dextra-sw.com>
2019-08-07 10:27:30 -0300

    Cleaning up some things that aren't gonna be used

--- server/py/splitter.py
+++ server/py/splitter.py
@@ -17,6 +17,12 @@ if video_stream is None:
 
 duration = float(video_stream['duration'])
 
-for x in range(int(duration // segment_duration)):
+int_segments = int(duration // segment_duration)
+last_segment_duration = duration % segment_duration
+
+for x in range(int_segments):
     ffmpeg.input('../video-in/attack.mp4', ss=x*segment_duration, t=segment_duration).output('out' + str(x) + '.mp4').run()
 
+if last_segment_duration > 1:
+    print('from' + str((int_segments+1) * segment_duration))
+    print('for ' + str(last_segment_duration) + 'secs')
