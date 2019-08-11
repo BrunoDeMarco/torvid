0a73199f8360c5c52223d2c6cd43001b86ed1bb2
Bruno de Marco <bruno.appolonio@dextra-sw.com>
2019-08-11 13:30:45 -0300

    Cleaning up a bit

--- server/api/Torvid.py
+++ server/api/Torvid.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, send_file
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/video', methods=['GET'])
+@cross_origin(origin='localhost',headers=['Content-Type','Authorization'])
+def video():
+    return send_file('../video-in/attack-fragmented.mp4')
